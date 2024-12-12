adb496d45f7949a4dc6bee5b14ddcd03761f4474
shakeer7396 <shakeer043@gmail.com>
2024-12-12 17:31:25 +0530

    input testing

--- ReactTesting/blog-app/src/App.js
+++ ReactTesting/blog-app/src/App.js
@@ -6,10 +6,12 @@ function App() {
       {/* You can add anything in start and end ogf the given text but not middle expected text is matching or not it will check these things */}
       <p>ABC First React test Case ABC123a</p>
       <p>Software Testing</p>
-      <img
-        title="GoogleImage"
+      <img   title="GoogleImage"
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrfwAhgmyiHHQ7kj-aEbk9AGCKcwo0wYeuwg&s"
       />
+      <br />
+      <br />
+      <input type='text' placeholder="Enter User Name" name='username' value='Shaik Shafi' id='userId' />
     </div>
   );
 }
--- ReactTesting/blog-app/src/App.test.js
+++ ReactTesting/blog-app/src/App.test.js
@@ -11,3 +11,16 @@ const text2 = screen.getByText(/Software Testing/i);
   expect (text2).toBeInTheDocument();
 
 });
+
+test("Testing input box", ()=>{
+  render(<App />)
+  let checkInput = screen.getByRole("textbox"); //input is showing or not checking
+  let checkInputPlaceHolder=screen.getByPlaceholderText("Enter User Name")
+
+  expect(checkInput).toBeInTheDocument();
+  expect(checkInputPlaceHolder).toBeInTheDocument();
+  expect(checkInput).toHaveAttribute("name","username");
+  expect(checkInput).toHaveAttribute("id","userId");
+  expect(checkInput).toHaveAttribute("type","text");
+  expect(checkInput).toHaveAttribute("value","Shaik Shafi");
+})
