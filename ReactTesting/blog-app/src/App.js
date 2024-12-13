20a7ff7f75016ef58e6ccd0566172275663b213f
shakeer7396 <shakeer043@gmail.com>
2024-12-13 10:16:30 +0530

    Re-Change to Single component

--- ReactTesting/blog-app/src/App.js
+++ ReactTesting/blog-app/src/App.js
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="App">
       {/* You can add anything in start and end ogf the given text but not middle expected text is matching or not it will check these things */}
+
+      {/*Checking with screen Reading text */}
       <p>ABC First React test Case ABC123a</p>
       <p>Software Testing</p>
       <img   title="GoogleImage"
@@ -11,6 +13,8 @@ function App() {
       />
       <br />
       <br />
+      
+      {/*Checking with the input box Testing*/}
       <input type='text' placeholder="Enter User Name" name='username' value='Shaik Shafi' id='userId' />
     </div>
   );
--- ReactTesting/blog-app/src/App.test.js
+++ ReactTesting/blog-app/src/App.test.js
@@ -1,6 +1,7 @@
 import {render, screen} from "@testing-library/react";
 import App from "./App";
 
+{/*Checking with screen Reading text */}
 test("Test First React Test Case", ()=>{
   render(<App />)
   const text = screen.getByText(/First React Test Case/i); //Its a case sensitive but using /i acpt C&c
@@ -12,6 +13,7 @@ const text2 = screen.getByText(/Software Testing/i);
 
 });
 
+{/*Checking with the input box Testing*/}
 test("Testing input box", ()=>{
   render(<App />)
   let checkInput = screen.getByRole("textbox"); //input is showing or not checking
