be6ffcad95cbac402a78a772de1e874bfa6cb57c
shakeer7396 <shakeer043@gmail.com>
2024-12-16 22:48:28 +0530

    commands

--- ReactTesting/blog-app/src/App.test.js
+++ ReactTesting/blog-app/src/App.test.js
@@ -2,5 +2,5 @@ import {render} from "@testing-library/react";
 import App from "./App";
 test("Snapshot Tesing",()=>{
   const container = render(<App />)
-  expect(container).toMatchSnapshot();
+  expect(container).toMatchSnaoshot();
 })
--- Zero_ProTs/app.js
+++ Zero_ProTs/app.js
@@ -1,18 +1,5 @@
-"use strict";
-class BottleMaker {
-    constructor() {
-        this.name = "Milton";
-    }
+function greet(name) {
+    if (name === void 0) { name = "Guest"; }
+    console.log("Hello, ".concat(name));
 }
-class MetalBottleMaker extends BottleMaker {
-    constructor() {
-        super(...arguments);
-        this.material = "metal";
-    }
-    changeName() {
-        this.name = "Some Name";
-    }
-}
-let b3 = new MetalBottleMaker();
-console.log(b3);
-// b3.name = "Some other name";  // there shows error outside the class not accessable
+greet();
