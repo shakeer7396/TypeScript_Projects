37f78cc2c0008393d8ea1d09961ecbf688131450
shakeer7396 <shakeer043@gmail.com>
2024-12-25 19:31:21 +0530

    Revision

--- ReactTesting/blog-app/src/App.js
+++ ReactTesting/blog-app/src/App.js
@@ -3,21 +3,10 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1> Query With getByText</h1>
-      <button>Click Me</button>
-      <h1 id="h1_tag">Get By Text</h1>
-      <p className="ptag" id="p_tag">
-        Testing
-      </p>
-
-      {/* <h1> Query getAllByText</h1>
-      <button>Click Me</button>
-      <h1>Get By Text</h1>
-      <p>Testing</p>
-
-      <button>Click Me</button>
-      <h1>Get By Text</h1>
-      <p>Testing</p> */}
+      <h1> Query With getByTestId</h1>
+      {/* Every element have unique id its first priority means id="divTag" but adding another attribute id for any other purpose like (data-testid="Anything add") you Over ride this like (element-testid="div-test-id") then use configure in testing file then only it will work */}
+      <div id="divTag" element-testid="div-test-id" >Div Testing With TestId</div>
+         
     </div>
   );
 }
--- ReactTesting/blog-app/src/App.test.js
+++ ReactTesting/blog-app/src/App.test.js
@@ -1,29 +1,11 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, configure } from "@testing-library/react";
 import App from "./App";
 
-test("Testing with getByText for single element", () => {
-  render(<App />);
-  const text = screen.getByText("Click Me");
-  const h1 = screen.getByText("Get By Text");
-  const p = screen.getByText("Testing");
-  expect(text).toBeInTheDocument();
-  expect(h1).toBeInTheDocument();
-  expect(p).toBeInTheDocument();
-  expect(p).toHaveClass("ptag");
-  expect(h1).toHaveAttribute("id");
-  expect(p).toHaveAttribute("id","p_tag");
-});
+configure({testIdAttribute:"element-testid"}); // App custom id use karna haito ya ha configure karna jaroori hai
 
-// If you want write seperate test cases for h1 & p
-// test("Testing with getAllByText for multiple element",()=>{
-//   render(<App />)
-//   const text=screen.getAllByText("Click Me");
-//   const h1 =screen.getAllByText("Get By Text")
-//   const p =screen.getAllByText("Testing")
-//   for(let i=0;i<text.length;i++){
-//     expect(text[i]).toBeInTheDocument();
-//     expect(h1[i]).toBeInTheDocument();
-//     expect(p[i]).toBeInTheDocument();
-//   }
+test("getByTestId testing with id in div",()=>{
+  render(<App />)
+  const div1 = screen.getByTestId("div-test-id");
+  expect(div1).toBeInTheDocument();
+})
 
-// })
